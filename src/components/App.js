344a5e25b4aa7555b2dc2672241f449747e9264e
Mukul Jataria <mukuljataria0723@gmail.com>
2022-07-14 16:55:14 +0530

    SUBSCRIBE TO THE STORE

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,29 @@ import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 
-function App() {
+class App extends React.Component {
+
+  componentDidMount(){
+    const {store} = this.props;
+    store.subscribe(()=>{
+      console.log('UPDATED');
+      this.forceUpdate();
+    });
+    //make api call
+    //dispatch action
+    store.dispatch({
+      type: 'ADD_MOVIES',
+      movies: data
+    });
+    console.log('STATE', store.getState());
+  }
+
+
+
+
+  render() {
+    const movies = this.props.store.getState();
+    console.log("RENDER");
   return (
     <div className="App">
        <Navbar />
@@ -14,8 +36,8 @@ function App() {
         </div>
 
         <div className="list">
-          {data.map(movie =>(
-            <MovieCard movie={movie} />
+          {movies.map((movie,index) =>(
+            <MovieCard movie={movie} key={'movies-${index}'} />
           ))}
 
         </div>
@@ -23,6 +45,8 @@ function App() {
        </div>
     </div>
   );
+  }
+  
 }
 
 
